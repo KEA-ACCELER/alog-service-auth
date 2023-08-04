5b052372394ac9dbcb8d5b7fe9db6a7fdd2dbd39
doyeon <sunmyoung61@gmail.com>
2023-08-04 16:01:15 +0900

    fix : listenting port (#8)
    * add : #2 #3 init
    
    * #2 #3 #6 add : Login and Register
    
    * fix : listenting port

--- src/main/java/kea/alog/auth/AuthApplication.java
+++ src/main/java/kea/alog/auth/AuthApplication.java
@@ -1,16 +1,20 @@
 package kea.alog.auth;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+		SpringApplication app = new SpringApplication(AuthApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8087"));
+		app.run(args);
 	}
 
 }
